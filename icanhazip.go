a538996f1a7b7502b90bfc5d83e5fa3c799310a3
Tyler Tidman <tyler.tidman@draak.ca>
2022-07-05 18:27:38 -0400

    Capture possible fix for icanhazip

--- icanhazip.go
+++ icanhazip.go
@@ -22,3 +22,7 @@ func moo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(requesterIpAndPort)
 	w.Write([]byte(requesterIp + "\n"))
 }
+
+// XXX FIXME TODO  Allow it to work on something other than 127.0.0.1 or ::1
+// https://gist.github.com/2minchul/191716b3ca8799f53362746731d08e91
+// https://gist.github.com/bacher09/51ce161105a9e1f49b8b917f8eccd3c5
