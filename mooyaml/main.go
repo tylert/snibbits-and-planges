f24030a05556266f71e393702c317da53ebee879
Tyler Tidman <tyler.tidman@draak.ca>
2022-10-28 13:57:25 -0400

    Prepare to add CSV to the mix

--- mooyaml/main.go
+++ mooyaml/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	// "encoding/csv"
 	// "encoding/json"
 	"fmt"
 	"os"
 	// "github.com/goccy/go-yaml"
+	// "github.com/jszwec/csvutil"
 )
 
 func main() {
@@ -13,6 +15,17 @@ func main() {
 		fmt.Println(GetVersion())
 		os.Exit(0)
 	}
+
+	// if aImport != "" {
+	//   file, err := os.Open(aImport)
+	//   if err != nil {
+	//     panic(err)
+	//   }
+
+	//   reader := csv.NewReader(file)
+	//   records, _ := reader.ReadAll()
+	//   fmt.Println(records)
+	// }
 
 	// bs, err := json.MarshalIndent(foo, "", "  ")
 	// if err != nil {
