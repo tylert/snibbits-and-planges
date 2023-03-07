e7b59f0c4df3ce05ce6819230e0f11b1bad79627
Tyler Tidman <tyler.tidman@draak.ca>
2023-03-06 21:22:54 -0500

    Prep slipping in new codec magic

--- shortuuid/codec.go
+++ shortuuid/codec.go
@@ -3,13 +3,13 @@ package main
 import (
 	"github.com/btcsuite/btcd/btcutil/base58"
 	"github.com/google/uuid"
+	// "github.com/tv42/base58"
+	// "github.com/tv42/zbase32"
 )
 
 // Other encodings???
 //   https://datatracker.ietf.org/doc/html/draft-msporny-base58-03
 //   https://stackoverflow.com/questions/41996761/golang-number-base-conversion/48362821#48362821
-//   https://github.com/tv42/base58
-//   https://github.com/tv42/zbase32
 //   https://github.com/Dasio/base45/blob/main/base45.go
 //   https://cs.opensource.google/go/go/+/master:src/encoding/base64/base64.go
 //   https://www.rfc-editor.org/info/rfc9285
