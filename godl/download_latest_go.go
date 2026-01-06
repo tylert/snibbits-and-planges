20461cc82d887885774661e952e4b17c9c7c8182
Tyler Tidman <tyler.tidman@draak.ca>
2026-01-06 12:05:28 -0500

    Hash the downloaded file, start prepping for fetching into a specified dir

--- godl/download_latest_go.go
+++ godl/download_latest_go.go
@@ -1,14 +1,27 @@
 /*usr/bin/env go run "$0" "$@"; exit;*/
 
+/* Download the latest version of the Go compiler suite via HTTP, verify the
+   checksum and extract it so we can start using it. */
+
+/* XXX FIXME TODO
+- Waaaaay better checking of the desired dest location and filename
+- Check if the latest version is already installed so we can skip unnecessary downloading
+- Allow picking os and arch to fetch
+- Unzip the archive somewhere in the $PATH like "/usr/local/"
+- Find a better way than printf to report download progress
+*/
+
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"path"
+	//"path"
+	//"path/filepath"
 	"strings"
 	"time"
 
@@ -20,17 +33,21 @@ func main() {
 }
 
 func FetchLatestGo() {
-	link, checksum := FindLatestGo("linux", "amd64")
-	fmt.Println(link)
+	// Figure out the link to the latest version (and the expected checksum)
+	url, checksum := DetermineLatestGo("linux", "amd64")
+	fmt.Println(url)
 	fmt.Println(checksum)
 
-	FetchFile(link)
-	// XXX FIXME TODO  Check the downloaded file against the expected checksum
-	// XXX FIXME TODO  Unzip the archive and provide a way to set the location
+	// Download the latest version from the provided link
+	dest := FetchFile(url, "/tmp/foo.tar.gz")
+
+	// Verify the checksum against the one we are expecting
+	hash := HashFileSHA256(dest)
+	fmt.Println(hash)
 }
 
-func FindLatestGo(os string, arch string) (string, string) {
-	// Fetch the webby stuff
+func DetermineLatestGo(os string, arch string) (string, string) {
+	// Do a bit of web-scraping to determine the latest version checksum
 	client := http.Client{
 		Timeout: 5 * time.Second,
 	}
@@ -64,11 +81,16 @@ func FindLatestGo(os string, arch string) (string, string) {
 		return true
 	})
 
+	// We don't know how to download nothing
+	if link == "" {
+		log.Fatal(fmt.Sprintf("Can't find an archive link for os=%s, arch=%s.", os, arch))
+	}
+
 	return link, checksum
 }
 
-func FetchFile(url string) {
-	// Connect to the http(s) endpoint
+func FetchFile(url string, dest string) string {
+	// Connect to the desired endpoint
 	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
@@ -78,13 +100,25 @@ func FetchFile(url string) {
 		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
 	}
 
-	// Create the file locally
-	filepath := path.Base(res.Request.URL.String())
-	out, err := os.OpenFile(filepath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	/*
+	     dir := path.Dir(dest)
+	     file := path.Base(dest)
+	     if strings.HasSuffix(dest, "/") {
+	       dir = dest
+	   		file = path.Base(res.Request.URL.String())
+	     }
+	     fmt.Println(dir)
+	     fmt.Println(file)
+	   	fullpath := filepath.Join(dir, file)
+	*/
+	fullpath := dest
+
+	// Create the local file
+	fh, err := os.OpenFile(fullpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer out.Close()
+	defer fh.Close()
 
 	// Write to the buffer in 32 kB chunks and count downloaded bytes
 	buffer := make([]byte, 32*1024)
@@ -93,7 +127,7 @@ func FetchFile(url string) {
 	for {
 		n, err := res.Body.Read(buffer)
 		if n > 0 {
-			_, writeErr := out.Write(buffer[:n])
+			_, writeErr := fh.Write(buffer[:n])
 			if writeErr != nil {
 				log.Fatal(err)
 			}
@@ -108,4 +142,21 @@ func FetchFile(url string) {
 		}
 	}
 	fmt.Printf("\n")
+
+	return fullpath
+}
+
+func HashFileSHA256(file string) string {
+	fh, err := os.Open(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fh.Close()
+
+	hash := sha256.New()
+	if _, err := io.Copy(hash, fh); err != nil {
+		log.Fatal(err)
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil))
 }
